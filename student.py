ec5968800a034514aa17c60c181dcf4f03c3cd0f
Sagnik0684 <sagnik0684@gmail.com>
2022-12-30 21:04:27 +0530

    Student examination portal

--- student.py
+++ student.py
@@ -1,56 +1,85 @@
-class Student:
-  def init(self, si, n, cn, bn):
-    self.si = si
-    self.n = n
-    self.cn = cn
-    self.bn = bn
-    self.m = {}
+import mysql.connector
+import json
+mydb = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "student")
+cursorObject = mydb.cursor()
 
-  def update_student(self, n=None, cn=None, bn=None):
-    if n:
-      self.n = n
-    if cn:
-      self.cn = cn
-    if bn:
-      self.bn = bn
+def createStudent(student_id, name):
+    class_roll_number = int(student_id[5:7])
+    batch_id = student_id[:5]
+    sql = "INSERT INTO student (student_id, name, class_roll_number, batch_id) VALUES (%s, %s, %s, %s)"
+    val = (student_id, name, class_roll_number, batch_id)
+    cursorObject.execute(sql, val)
+    mydb.commit()
 
-  def remove_student(self):
-    # remove student from database
-    pass
+def updateStudent(ostudent_id, nstudent_id, name):
+    class_roll_number = int(nstudent_id[5:7])
+    batch_id = nstudent_id[0:5]
+    sql = "UPDATE student SET student_id = %s, name = %s, class_roll_number = %s, batch_id = %s WHERE student_id = %s"
+    val = (nstudent_id, name, class_roll_number, batch_id, ostudent_id)
+    cursorObject.execute(sql, val)
+    mydb.commit()
 
-  def generate_report_card(self):
-    total_marks = 0
-    total_subjects = 0
-    for subject, m in self.m.items():
-      total_marks += m
-      total_subjects += 1
+def removeStudent(student_id):
+    sql = "DELETE FROM student WHERE student_id = %s"
+    val = (student_id, )
+    cursorObject.execute(sql, val)
+    mydb.commit()
 
-    p = total_marks / total_subjects
-    if p >= 90:
-      grade = 'A'
-    elif p >= 80:
-      grade = 'B'
-    elif p >= 70:
-      grade = 'C'
-    elif p >= 60:
-      grade = 'D'
-    elif p >= 50:
-      grade = 'E'
+def reportCard(student_idx):
+    cursorObject.execute("SELECT * FROM student")
+    result = cursorObject.fetchall()
+    students = []
+    student_ids = []
+    for i in range(0, len(result)):
+        student_ids.append(result[i][0])
+        students.append(result[i][1])
+    check = 0
+    for i in range(0, len(student_ids)):
+        if(student_idx == student_ids[i]):
+            check = 1
+            break
+    if(check == 1):
+        studentx = students[i]
+        mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "course")
+        cursorObject1 = mydb1.cursor()
+        cursorObject1.execute("SELECT * FROM course")
+        result = cursorObject1.fetchall()
+        marks = []
+        subjects = []
+        for i in range(0, len(result)):
+            marks.append(json.loads(result[i][2]))
+            subjects.append(result[i][1])
+        file_name = student_idx + ".txt"
+        file = open(file_name, "w+")
+        student_id = "Student ID: " + student_idx + " \n"
+        name = "Name: " + studentx + " \n"
+        L = [student_id, name]
+        file.writelines(L)
+        total_marks = 0
+        divs = 0
+        for i in range(0, len(subjects)):
+            temp = marks[i]
+            if(isinstance(temp.get(student_idx), int)):
+                subject_marks = "Marks in " + subjects[i] + ": " + str(temp.get(student_idx)) + "% \n"
+                divs += 1
+                total_marks += temp.get(student_idx)
+                file.write(subject_marks)
+        grade = "Grade obtained: " + gradeCheck(total_marks/divs) + " \n"
+        file.write(grade)
+        file.close()
     else:
-      grade = 'F'
+        print("Student ID does not exist")
 
-    result = 'PASS' if grade != 'F' else 'FAIL'
-
-    report_card = f'Percentage: {p}\nGrade: {grade}\nResult: {result}'
-
-    # write report_card to text file
-    with open(f'{self.n}_report_card.txt', 'w') as f:
-      f.write(report_card)
-student1 = Student(1, 'John', 2, '2022')
-student1.update_student(n='John Smith', cn=3)
-student1.remove_student()
-student1.m['Maths'] = 85
-student1.m['Science'] = 90
-student1.m['English'] = 95
-
-student1.generate_report_card()
+def gradeCheck(a):
+    if(a >= 90):
+        return "A"
+    elif(a >= 80):
+        return "B"
+    elif(a >= 70):
+        return "C"
+    elif(a >= 60):
+        return "D"
+    elif(a >= 50):
+        return "E"
+    else:
+        return "F"
